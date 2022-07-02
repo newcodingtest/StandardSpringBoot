2f7ac24a1066ac85c1262a95034e4b69234a470e
jyyoon@ibleaders.co.kr <jyyoon@ibleaders.co.kr>
2022-07-03 01:31:43 +0900

    초기세팅

---
+++ src/test/java/com/yoon/standard/StandardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.standard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
