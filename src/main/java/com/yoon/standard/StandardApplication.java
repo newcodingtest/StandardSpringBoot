a1ebff743058f1c2e100397c36b09e17ee0c1fe0
윤주영 <jyyoon@ibleaders.co.kr>
2022-07-03 01:53:21 +0900

    test

--- src/main/java/com/yoon/standard/StandardApplication.java
+++ src/main/java/com/yoon/standard/StandardApplication.java
@@ -2,12 +2,25 @@ package com.yoon.standard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @SpringBootApplication
-public class StandardApplication {
+public class StandardApplication implements ApplicationListener<ApplicationEvent> {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StandardApplication.class, args);
+	}
+
+	/**
+	 * 
+	 * */
+	@Override
+	public void onApplicationEvent(ApplicationEvent event) {
+		
 	}
 
 }
