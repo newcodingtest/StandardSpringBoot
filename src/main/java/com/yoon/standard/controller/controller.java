3970285c2cae07bb339b35c90e2dc5c1b10de111
윤주영 <jyyoon@ibleaders.co.kr>
2022-07-03 06:35:00 +0900

    + WEB 컨트롤러 Unit Test 적용

---
+++ src/main/java/com/yoon/standard/controller/controller.java
@@ -0,0 +1,24 @@
+package com.yoon.standard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class controller {
+	
+	@GetMapping
+	public String getIndex(String data, ModelMap modelMap) {
+		
+		modelMap.addAttribute("data","GET 테스트 " + data);
+		return "index";
+	}
+	
+	@PostMapping
+	public String postIndex(String data, ModelMap modelMap) {
+		
+		modelMap.addAttribute("data","POST 테스트 " + data);
+		return "index";
+	}
+}
---
+++ src/test/java/com/yoon/standard/domain/controller/web/TestBaseController.java
@@ -0,0 +1,75 @@
+package com.yoon.standard.domain.controller.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@SpringBootTest
+@ActiveProfiles({"dev", "db-h2"}) //테스트에 적용할 설정파일 적용
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) //테스트 순서 적용 어노테이션
+public class TestBaseController {
+
+	@Autowired
+	MockMvc mockMvc;
+	
+	@Test
+	@Order(1)
+	public void getIndex() throws Exception {
+		//GIVEN
+		String data = "GET";
+		
+		//WHEN
+		MultiValueMap<String, String>params = new LinkedMultiValueMap<>();
+		params.add("data", data);
+		
+		MvcResult mvcResult = mockMvc.perform(get("/").params(params))
+				.andDo(print())
+				.andExpect(status().is(HttpStatus.OK.value()))
+				.andReturn();
+		
+		//THEN
+		String content = mvcResult.getResponse().getContentAsString();
+		Assertions.assertThat(content).contains(data);
+	}
+	
+	@Test
+	@Order(2)
+	public void postIndex() throws Exception {
+		//GIVEN
+		String data = "POST";
+		
+		//WHEN
+		MultiValueMap<String, String>params = new LinkedMultiValueMap<>();
+		params.add("data", data);
+		
+		MvcResult mvcResult = mockMvc.perform(post("/").params(params))
+				.andDo(print())
+				.andExpect(status().is(HttpStatus.OK.value()))
+				.andReturn();
+		
+		//THEN
+		String content = mvcResult.getResponse().getContentAsString();
+		Assertions.assertThat(content).contains(data);
+	}
+}
