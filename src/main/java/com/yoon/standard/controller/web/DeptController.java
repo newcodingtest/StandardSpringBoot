9f1d6169d0878607faf4f1c62565bdb1cbbccb3b
윤주영 <jyyoon@ibleaders.co.kr>
2022-07-04 02:06:49 +0900

    + 기본 CRUD 로직 UI 구현

--- src/main/java/com/yoon/standard/controller/web/DeptController.java
+++ src/main/java/com/yoon/standard/controller/web/DeptController.java
@@ -1,9 +1,12 @@
 package com.yoon.standard.controller.web;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller; 
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.yoon.standard.controller.dto.RequestDeptDto;
 import com.yoon.standard.service.dept.DeptService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,5 +22,30 @@ public class DeptController {
 	public String deptList(ModelMap modelMap)throws Exception {
 		modelMap.addAttribute("deptList", deptService.deptList());
 		return "deptList";
+	}
+	
+	@GetMapping(value ="/dept/{deptno}")
+	public String deptDetail(@PathVariable("deptno") int deptno, ModelMap modelMap)throws Exception {
+		modelMap.addAttribute("dept",deptService.deptDetail(deptno));
+		return "deptDetail";
+	}
+	
+	@PostMapping(value ="/dept")
+	public String deptInsert(RequestDeptDto requestDeptDto)throws Exception {
+		deptService.deptInsert(requestDeptDto.toEntity());
+		return "redirect:/dept";
+	}
+	
+	@PostMapping(value ="/dept/update")
+	public String deptUpdate(RequestDeptDto requestDeptDto)throws Exception {
+		deptService.deptUpdate(requestDeptDto.toEntity());
+		return "redirect:/dept";
+	}
+	
+	
+	@PostMapping(value ="/dept/delete")
+	public String deptDelte(Integer deptno)throws Exception {
+		deptService.deptDelete(deptno);
+		return "redirect:/dept";
 	}
 }
